c8029057d122ab00e783f8bb8175627e3efd32da
Tanmay Madambi <tanmay.madambi@gmail.com>
2026-04-09 10:17:14 +0530

    microservice assignment

---
+++ 06-04-2026/Recommendation/src/main/java/com/example/demo/RecommendationApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate gestRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
